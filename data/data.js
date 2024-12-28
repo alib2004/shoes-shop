b6d3c2c81122c2d7505c4e9c87ce630aaca8617e
Ali Beygholeh <129377168+alib2004@users.noreply.github.com>
2024-12-28 11:41:47 +0330

    Update data.js

--- data/data.js
+++ data/data.js
@@ -1,7 +1,7 @@
 export const shoes = [
   {
     id: 1,
-    image: "../data/img/1.jpg",
+    image: "https://alib2004.github.io/shoes-shop/data/img/1.jpg",
     title: "Chaussure Campus 00s",
     price : 19,
     brand: "adidas",
@@ -9,7 +9,7 @@ export const shoes = [
   },
   {
     id: 2,
-    image: "../data/img/2.jpg",
+    image: ".https://alib2004.github.io/shoes-shop/data/img/2.jpg",
     title: "Chaussure Samba OG",
     price : 80.55,
     brand: "adidas",
@@ -17,7 +17,7 @@ export const shoes = [
   },
   {
     id: 3,
-    image: "../data/img/3.jpg",
+    image: "https://alib2004.github.io/shoes-shop/data/img/3.jpg",
     title: "Chaussure Handball Spezial",
     price : 40,
     brand: "adidas",
